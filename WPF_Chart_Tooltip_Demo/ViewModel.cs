b8e1c25805d4a04b9b61558c0eca09b6a2532a1e
VimalaThirumalaiKumarSF3739 <vimala.t@syncfusion.com>
2023-02-15 11:39:44 +0530

    Added sample with new name

---
+++ WPF_Chart_Tooltip_Demo/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chart_Tooltip_Demo
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>()
+            {
+                new Model() {XValue="Jan", YValue = 20},
+                new Model() {XValue="Feb",YValue = 40},
+                new Model() {XValue="Mar",YValue = 60},
+                new Model() {XValue= "Apr",YValue = 80},
+                new Model() {XValue="May",YValue = 100},
+            };
+
+        }
+    }
+
+    public class Model
+    {
+        public string XValue { get; set; }
+        public double YValue { get; set; }
+    }
+}
